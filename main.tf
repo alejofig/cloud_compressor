e869fd9d5f75dd83068fdfcac1d44433d09d4299
Alejandro Figueroa <alejofig@hotmail.com>
2023-04-19 19:21:24 -0500

    gcp

--- main.tf
+++ main.tf
@@ -10,11 +10,33 @@ locals {
   zone = data.google_compute_zones.available.names[0]
 }
 # Creación de red
-resource "google_compute_network" "my-network2" {
-  name = "my-network2"
+resource "google_compute_network" "my-network" {
+  name = "my-network"
   auto_create_subnetworks = true
   }
 
+resource "google_compute_instance" "redis" {
+  name         = "redis-instance"
+  machine_type = "n1-standard-1"
+  zone         = local.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-11"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.my-network.self_link
+  }
+
+  metadata_startup_script = <<-EOF
+    sudo apt-get update
+    sudo apt-get install -y redis-server
+    sudo sed -i 's/bind 127.0.0.1 ::1/bind 0.0.0.0 ::1/' /etc/redis/redis.conf
+    sudo systemctl restart redis.service
+  EOF
+}
 # Creación de instancia de máquina virtual "web"
 resource "google_compute_instance" "web" {
   name         = "web"
@@ -26,10 +48,10 @@ resource "google_compute_instance" "web" {
       image = "debian-cloud/debian-11"
     }
   }
-
+  depends_on = [google_compute_instance.redis, google_compute_instance.database]
   # Conexión a la red
   network_interface {
-    network = google_compute_network.my-network2.self_link
+    network = google_compute_network.my-network.self_link
         access_config {
       nat_ip = google_compute_address.web.address
     }
@@ -38,10 +60,10 @@ resource "google_compute_instance" "web" {
   metadata_startup_script = <<-EOF
     #!/bin/bash
     echo "empezando el script"
-    export DATABASE_URL="postgresql://example:example@34.29.28.151:5432/example"
-    export CELERY_RESULT_BACKEND="redis://redis:6379/0"
+    export DATABASE_URL="postgresql://example:example@${google_compute_instance.database.network_interface.0.access_config.0.nat_ip}:5432/example"
+    export CELERY_RESULT_BACKEND=redis://redis:6379/0
     export PUERTO_SMTP=587
-    export CELERY_BROKER_URL="redis://redis:6379/0"
+    export CELERY_BROKER_URL=redis://redis:6379/0
     export CORREO_ELECTRONICO="clouduniandesmiso@hotmail.com"
     export SERVIDOR_SMTP="smtp-mail.outlook.com"
     export USUARIO_SMTP="clouduniandesmiso@hotmail.com"
@@ -69,7 +91,7 @@ resource "google_compute_address" "web" {
 
 resource "google_compute_firewall" "allow-http-web" {
   name    = "allow-http-web"
-  network = google_compute_network.my-network2.self_link
+  network = google_compute_network.my-network.self_link
 
   allow {
     protocol = "tcp"
@@ -82,7 +104,7 @@ resource "google_compute_firewall" "allow-http-web" {
 
 resource "google_compute_firewall" "allow-ssh-web" {
   name    = "allow-ssh-web"
-  network = google_compute_network.my-network2.self_link
+  network = google_compute_network.my-network.self_link
 
   allow {
     protocol = "tcp"
@@ -97,3 +119,79 @@ resource "google_compute_firewall" "allow-ssh-web" {
 output "web_ip_address" {
   value = google_compute_instance.web.network_interface.0.access_config.0.nat_ip
 }
+
+output "redis_ip_address" {
+  value = google_compute_instance.redis.network_interface.0.network_ip
+}
+
+
+
+####### DATABASE
+
+
+resource "google_compute_address" "database" {
+  name = "database-ip"
+}
+
+resource "google_compute_instance" "database" {
+  name         = "database-address"
+  machine_type = "n1-standard-1"
+  tags         = ["database"]
+  zone         = local.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  # Conexión a la red
+  network_interface {
+    network = google_compute_network.my-network.self_link
+
+    access_config {
+      # Asigna la dirección IP pública a la instancia
+      nat_ip = google_compute_address.database.address
+    }
+  }
+
+  # Configuración de PostgreSQL para habilitar el acceso remoto a la base de datos
+  metadata_startup_script = <<-EOF
+    #!/bin/bash
+    sudo apt-get update
+    sudo apt-get -y install postgresql postgresql-contrib
+
+    # Habilita la escucha de PostgreSQL en todas las interfaces de red
+    sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/13/main/postgresql.conf
+
+    # Agrega la regla de acceso en pg_hba.conf para permitir el acceso remoto a PostgreSQL
+    sudo sh -c "echo 'host all all 0.0.0.0/0 md5' >> /etc/postgresql/13/main/pg_hba.conf"
+
+    # Reinicia el servicio de PostgreSQL
+    sudo service postgresql restart
+
+    # Crea un usuario y una base de datos de ejemplo en PostgreSQL
+    sudo -u postgres psql -c "CREATE USER example WITH PASSWORD 'example';"
+    sudo -u postgres psql -c "CREATE DATABASE example OWNER example;"
+  EOF
+}
+
+resource "google_compute_firewall" "allow-postgres-db" {
+  name    = "allow-postgres-db"
+  network = google_compute_network.my-network.self_link
+
+  allow {
+    protocol = "tcp"
+    ports    = ["5432","22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags = [tolist(google_compute_instance.database.tags)[0]]
+}
+
+output "database_ip_address" {
+  value = google_compute_instance.database.network_interface.0.access_config.0.nat_ip
+}
+
+
+###### END DATABASE
