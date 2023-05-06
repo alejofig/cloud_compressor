7af0c676f78df24eeafffc1e4cb8d608b42b6b76
Alejandro Figueroa <alejofig@hotmail.com>
2023-05-06 08:01:14 -0500

    lb

--- web/web.tf
+++ web/web.tf
@@ -14,13 +14,13 @@ zone = "us-central1-c"
 target = google_compute_instance_group_manager.foobar.self_link
 
 autoscaling_policy {
-max_replicas = 3
-min_replicas = 1
-cooldown_period = 60
+  max_replicas = 3
+  min_replicas = 1
+  cooldown_period = 60
 
-cpu_utilization {
-target = 0.6
-}
+  cpu_utilization {
+  target = 0.6
+  }
 }
 }
 
