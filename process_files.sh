05558f9e465c36bea29bebb829fe08cdf13ba7bd
Alejandro Figueroa <alejofig@hotmail.com>
2023-05-06 10:02:32 -0500

    falto r

--- process_files.sh
+++ process_files.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export DATABASE_URL=postgresql://postgres:example@34.71.237.78:5432/example
-export CELERY_RESULT_BACKEND=edis://35.226.66.8:6379/0
+export CELERY_RESULT_BACKEND=redis://35.226.66.8:6379/0
 export PUERTO_SMTP=587
-export CELERY_BROKER_URL=edis://35.226.66.8:6379/0
+export CELERY_BROKER_URL=redis://35.226.66.8:6379/0
 export CORREO_ELECTRONICO="clouduniandesmiso@hotmail.com"
 export SERVIDOR_SMTP="smtp-mail.outlook.com"
 export USUARIO_SMTP="clouduniandesmiso@hotmail.com"
