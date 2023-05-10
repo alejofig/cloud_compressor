ecaf38b1fc3fccb38af6cc358c37fcef09ca3d46
Alejandro Figueroa <alejofig@hotmail.com>
2023-05-09 22:33:39 -0500

    pubsub

--- web/web.tf
+++ web/web.tf
@@ -55,7 +55,7 @@ tags = ["web"]
     sudo curl -L "https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
     docker-compose --version
-    git clone -b bucket_jcra https://github.com/alejofig/cloud_compressor.git
+    git clone -b pubsub https://github.com/alejofig/cloud_compressor.git
     cd cloud_compressor
     sudo docker-compose up -d
     echo "Nuevo"
